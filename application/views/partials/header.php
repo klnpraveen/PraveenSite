6caa0d2c0ed0feb3a736cbb106a7d35ffdae5720
manoharthedeveloper <manohar@bootstrapguru.com>
2014-04-27 17:14:20 +0530

    save

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -13,63 +13,93 @@
 		'rel' => 'stylesheet',
 		'type' => 'text/css',
 		);
-	$link_fontawesome = array(
-		'href' => 'css/font-awesome.css',
-		'rel' => 'stylesheet',
-		'type' => 'text/css',
-		);
-	$link_fontawesomemin = array(
-		'href' => 'css/font-awesome.min.css',
+	$link_fstyle = array(
+		'href' => 'css/style_front.css',
 		'rel' => 'stylesheet',
 		'type' => 'text/css',
 		);
 	echo link_tag($link_bootstrap);
+	echo link_tag($link_fstyle);
 	echo link_tag($link_custom);
-	echo link_tag($link_fontawesome);
-	echo link_tag($link_fontawesomemin);
 	echo link_tag('images/favicon.ico', 'shortcut icon', 'image/ico');
 	?>
 </head>
 <body>
-	<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
-        </div>
-      </div>
+	<div class="site-holder">
+		<!-- .navbar -->
+		<nav class="navbar  navbar-default nav-delighted " role="navigation">
+			<div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+          <img src="images/doctor.png" alt="">
+      </a>
     </div>
 
-	
-	<div class="col-md-2 left-navbar   nav navbar-left">
-		<ul class="menu list-unstyled">
-			<li>Home</li>
-			<li>About Me</li>
-			<li>menu</li>
-			<li>Contact</li>
-			<li>Sign In</li>
-		</ul>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    
+      <ul class="nav navbar-nav navbar-right">
+ <li>
+                                    <form role="search" class="search-form">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control  nav-input-search " placeholder="Search ">
+                                            <i class="fa fa-search"></i>
+                                        </div>
+                                    </form>
+                                </li>
+                                <li>
+                                    <a href="lock.html" class="settings">
+                                        <i class="fa fa-cogs"></i>
+                                    </a>
+                                </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div>
+	</nav>                    
+
+	<!-- .left-sidebar -->
+	<div class="left-sidebar">
+		@Leftside Menu
 	</div>
-	<div class="col-md-10 content">
+
+	
+	<!-- .content -->
+	<div class=" breadcrumb-holder">
+		@Breadcrumb
+	</div>    
+
+	<div class="main-content">
+		@Main content
+
 		
-	
 
-	
 
-	
-	
+
+
+
+
 
 
